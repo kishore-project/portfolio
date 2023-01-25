9cdc83a22d7458b27d060b05e3f9c5a47e14b2d1
Kishore kumar <96686690+kishore-project@users.noreply.github.com>
2023-01-25 13:40:37 +0530

    Add files via upload

---
+++ src/data/SkillsData.js
@@ -0,0 +1,102 @@
+import {
+	FaBootstrap,
+	FaCss3Alt,
+	FaGithub,
+	FaHtml5,
+	FaNpm,
+	FaReact,
+	FaSass,
+	FaYarn,
+	FaFigma,
+} from "react-icons/fa";
+import { DiJavascript1 } from "react-icons/di";
+import { VscJson } from "react-icons/vsc";
+import {
+	SiAdobephotoshop,
+	SiJquery,
+	SiTailwindcss,
+	SiNextdotjs,
+	SiTypescript,
+	SiFirebase,
+	SiStyledcomponents,
+	SiFramer
+} from "react-icons/si";
+
+export const SkillsData = [
+	{
+		name: "HTML",
+		icon: <FaHtml5 />,
+	},
+	{
+		name: "CSS",
+		icon: <FaCss3Alt />,
+	},
+	{
+		name: "Javascript",
+		icon: <DiJavascript1 />,
+	},
+	{
+		name: "TypeScript",
+		icon: <SiTypescript />,
+	},
+	{
+		name: "jQuery",
+		icon: <SiJquery />,
+	},
+	{
+		name: "React Js",
+		icon: <FaReact />,
+	},
+	{
+		name: "Next Js",
+		icon: <SiNextdotjs />,
+	},
+	{
+		name: "Firebase",
+		icon: <SiFirebase />,
+	},
+	{
+		name: "JSON",
+		icon: <VscJson />,
+	},
+	{
+		name: "Bootstrap",
+		icon: <FaBootstrap />,
+	},
+	{
+		name: "Tailwind",
+		icon: <SiTailwindcss />,
+	},
+	{
+		name: "Sass",
+		icon: <FaSass />,
+	},
+	{
+		name: "Styled Components",
+		icon: <SiStyledcomponents />,
+	},
+	{
+		name: "Framer Motion",
+		icon: <SiFramer />,
+	},
+	{
+		name: "Git",
+		icon: <FaGithub />,
+	},
+	{
+		name: "Npm",
+		icon: <FaNpm />,
+	},
+	{
+		name: "Yarn",
+		icon: <FaYarn />,
+	},
+	{
+		name: "Figma",
+		icon: <FaFigma />,
+	},
+	{
+		name: "Photoshop",
+		icon: <SiAdobephotoshop />,
+	},
+];
